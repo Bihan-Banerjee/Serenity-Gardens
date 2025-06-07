930634842a1d636902dbb03b94ea5ad861bad739
Bihan-Banerjee <bihanbanerjee26@gmail.com>
2025-06-07 09:06:47 +0530

    cart error removed

--- client/src/pages/useCartStore.tsx
+++ client/src/pages/useCartStore.tsx
@@ -1,11 +1,26 @@
 import { create } from 'zustand';
 
-export const useCartStore = create((set) => ({
+type CartItem = {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+};
+
+interface CartState {
+  cart: CartItem[];
+  addItem: (item: CartItem) => void;
+  removeItem: (id: string) => void;
+  updateQuantity: (id: string, quantity: number) => void;
+  clearCart: () => void;
+}
+
+export const useCartStore = create<CartState>((set) => ({
   cart: [],
   addItem: (item) =>
     set((state) => {
-      const existing = state.cart.find((i) => i.id === item.id);
-      if (existing) {
+      const exists = state.cart.find((i) => i.id === item.id);
+      if (exists) {
         return {
           cart: state.cart.map((i) =>
             i.id === item.id ? { ...i, quantity: i.quantity + item.quantity } : i
@@ -15,15 +30,18 @@ export const useCartStore = create((set) => ({
         return { cart: [...state.cart, item] };
       }
     }),
+
   removeItem: (id) =>
     set((state) => ({
       cart: state.cart.filter((item) => item.id !== id),
     })),
+
   updateQuantity: (id, quantity) =>
     set((state) => ({
       cart: state.cart.map((item) =>
         item.id === id ? { ...item, quantity } : item
       ),
     })),
+
   clearCart: () => set({ cart: [] }),
 }));
