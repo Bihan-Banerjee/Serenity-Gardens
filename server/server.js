20b2a11e6321f201ea98b5082df54e14a06e4e37
Bihan-Banerjee <bihanbanerjee26@gmail.com>
2025-10-22 20:31:42 +0530

    cors error debugging

--- server/server.js
+++ server/server.js
@@ -35,7 +35,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use("/api/reviews", reviewRoutes);
